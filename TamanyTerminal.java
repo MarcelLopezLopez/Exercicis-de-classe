9f9b3440dd7bf9550b34ef7522fd0e5a9bb040b6
Marcel <marcelopez@estudiantat.upc.edu>
2023-10-30 12:22:25 +0100

    no finalitzat

--- TamanyTerminal.java
+++ TamanyTerminal.java
@@ -1,9 +1,23 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class TamanyTerminal {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        int columns;
+        try {
+
+            EditableBufferedReader.setRaw();
+
+            System.out.print("\033[18t");
+
+            Scanner sc = new Scanner(System.in);
+
+        } finally {
+
+        }
+        
         try {
             // Ejecutar el comando "tput cols" para obtener el número de columnas del terminal
             Process process = Runtime.getRuntime().exec(new String[] { "bash", "-c", "tput cols 2>/dev/tty" });
@@ -12,7 +26,7 @@ public class TamanyTerminal {
             BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
             String line;
             if ((line = reader.readLine()) != null) {
-                int columns = Integer.parseInt(line);
+                columns = Integer.parseInt(line);
                 System.out.println("Número de columnas del terminal: " + columns);
             }
 
