d96554546c8249af095fe53f60d5fadc0b1fff88
MarcelLopezLopez <marcelopez@estudiantat.upc.edu>
2023-10-31 16:57:06 +0100

    exercicis EchoClient i EchoServer

--- EchoClient.java
+++ EchoClient.java
@@ -1,9 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
 public class EchoClient {
 
     public static void main(String[] args) {
-        
+        // args[0] fara que agafem el primer parametre que li pasem despres d'executar
+        // el programa
+        // host //port
+        MySocket sc = new MySocket(args[0], Integer.parseInt(args[1]));
         // Input threat (keyboard)
-        new Threat()
+        new Thread() {
+            public void run() {
+                BufferedReader kbd = new BufferedReader(new InputStreamReader(System.in));
+                String line;
+                try {
+                    while ((line = kbd.readLine()) != null) {
+                        sc.println(line);
+                    }
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }.start();
+
         // Output threat (console)
+        new Thread() {
+            public void run() {
+                String line;
+                while ((line = sc.readLine()) != null) {
+                        System.out.println(line);
+                    }
+            }
+        }.start();
+
+        //Segona manera
+        // Output threat (console)
+        new Thread(() -> {
+            String line;
+            while ((line = sc.readLine()) != null) {    
+                System.out.println(line);
+                }
+        }).start;
     }
 }
---
+++ EchoServer.java
@@ -0,0 +1,9 @@
+public class EchoServer {
+    public static void main(String[] args) {
+        MyServerSocket ss = new MyServerSocket(Integer.parseInt(args[0]));
+
+        while(true){
+            
+        }
+    }
+}
