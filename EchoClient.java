20ed83a3be350cb8120195ff0fc53adbeb5efd94
Marcel <marcelopez@estudiantat.upc.edu>
2023-10-31 16:07:25 +0100

    .

---
+++ EchoClient.java
@@ -0,0 +1,9 @@
+public class EchoClient {
+
+    public static void main(String[] args) {
+        
+        // Input threat (keyboard)
+        new Threat()
+        // Output threat (console)
+    }
+}
